c7b6a5ae06c8f640b393c5ddfdb946f12ad98689
Kat <ekatanti@yandex.ru>
2020-09-30 18:39:57 +0300

    Popup настройки персонажа

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,10 @@ const WIZARD_AMOUNT = 4;
 const userDialog = document.querySelector(`.setup`);
 const similarWizards = document.querySelector(`.setup-similar`);
 const similarWizardsContainer = document.querySelector(`.setup-similar-list`);
+const userDialogOpen = document.querySelector(`.setup-open`);
+const userDialogClose = document.querySelector(`.setup-close`);
+const userDialogPlayer = document.querySelector(`.setup-player`);
+const userDialogPlayerName = document.querySelector(`input[name="username"]`);
 const fragment = document.createDocumentFragment();
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
   .content
@@ -49,6 +53,14 @@ const EYES_COLORS = [
   `green`
 ];
 
+const FIREBALL_COLORS = [
+  `#ee4830`,
+  `#30a8ee`,
+  `#5ce6c0`,
+  `#e848d5`,
+  `#e6e848`
+];
+
 const getRandomInteger = (min, max) => {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
@@ -86,7 +98,25 @@ const appendWizards = (wizardsArr) => {
   similarWizardsContainer.append(fragment);
 };
 
-userDialog.classList.remove(`hidden`);
+const onPopupEscPress = (evt) => {
+  if (evt.key === `Escape` && userDialogPlayerName !== document.activeElement) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const openPopup = () => {
+  userDialog.classList.remove(`hidden`);
+
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+const closePopup = () => {
+  userDialog.classList.add(`hidden`);
+
+  document.removeEventListener(`keydown`, onPopupEscPress);
+};
+
 similarWizards.classList.remove(`hidden`);
 
 for (let i = 0; i < WIZARD_AMOUNT; i++) {
@@ -95,4 +125,44 @@ for (let i = 0; i < WIZARD_AMOUNT; i++) {
 
 appendWizards(wizards);
 
+userDialogOpen.addEventListener(`click`, () => {
+  openPopup(userDialog);
+});
 
+userDialogOpen.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    openPopup(userDialog);
+  }
+});
+
+userDialogClose.addEventListener(`click`, () => {
+  closePopup(userDialog);
+});
+
+userDialogClose.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    closePopup(userDialog);
+  }
+});
+
+userDialogPlayer.addEventListener(`click`, (evt) => {
+  let newColor;
+
+  if (evt.target && evt.target.matches(`.setup-wizard .wizard-coat`)) {
+    newColor = getRandomFeature(COAT_COLORS);
+    evt.target.style.fill = newColor;
+    userDialogPlayer.querySelector(`input[name="coat-color"]`).value = newColor;
+  }
+
+  if (evt.target && evt.target.matches(`.setup-wizard .wizard-eyes`)) {
+    newColor = getRandomFeature(EYES_COLORS);
+    evt.target.style.fill = newColor;
+    userDialogPlayer.querySelector(`input[name="eyes-color"]`).value = newColor;
+  }
+
+  if (evt.target && evt.target.matches(`.setup-fireball`)) {
+    newColor = getRandomFeature(FIREBALL_COLORS);
+    evt.target.style.backgroundColor = newColor;
+    userDialogPlayer.querySelector(`input[name="fireball-color"]`).value = newColor;
+  }
+});
